eed8c2d122f92eb11220db1a4a18f51c60925109
maw0728 <maw0728@gmail.com>
2020-08-21 17:44:30 +0900

    2.4.1 감싸인 요소

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -1,22 +1,19 @@
-import React from "react";
-import "./App.css";
+import React /*,{ Fragment } Fragment 사용시*/ from "react";
 
 function App() {
   return (
+    // 컴포넌트에 여러 요소가 있으면 반드시 부모 요소 하나로 감싸야 한다
     <div>
-      Hello <b>react</b>
+      <h1>리액트 안녕</h1>
+      <h2>잘 작동하니?</h2>
     </div>
+    /*
+    리액트 v16 이상부터 div 대신 Fragment를 사용할수있다
+    <Fragment>
+      <h1>리액트 안녕</h1>
+      <h2>잘 작동하니?</h2>
+    </Fragment>
+    */
   );
 }
-/*
-JSX를 사용하지 않고 자바스크립트로만 리액트를 만들때
-function App() {
-  return React.createElement(
-    "div",
-    null,
-    "Hello ",
-    React.createElement("b", null, "react")
-  );
-}
-*/
 export default App;
