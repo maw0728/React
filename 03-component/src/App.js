20372aa2981d69a3afa72e8f64d6e286f5f296f8
maw0728 <maw0728@gmail.com>
2020-08-22 04:48:00 +0900

    3.2.3 모듈 내보내기 및 불러오기

--- 03-component/src/App.js
+++ 03-component/src/App.js
@@ -1,12 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
+import MyComponent from "./MyComponent";
 
-// 클래스형 컴포넌트에서는 render 함수가 꼭 있어야한다
-//함수형 컴포넌트는 선언하기가 쉬운 대신 state와 라이프사이클 API의 사용이 불가능하다
-class App extends Component {
-  render() {
-    const name = "react";
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
