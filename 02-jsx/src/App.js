fd0eec0ba05f2c314b6545c5f721e59f86213186
maw0728 <maw0728@gmail.com>
2020-08-22 04:04:03 +0900

    2.4.6 인라인 스타일링

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function App() {
-  const name = undefined;
-  // 잘못된예시
-  // return name; 함수에서 undefined만 반환하여 렌더링 하면 안 된다
-
-  // 작동가능한 예시
-  // return name || "값이 undefined입니다"; OR 연산자를 이용하여 값이 undfined때 사용할 값을 지정해주면 된다
-  // return <div>{name}</div>; JSX 내부에서 undefined를 렌더링하는 것은 괜찮습니다
-  return <div>{name || "리액트"}</div>; // name 값이 undefined일 때 보여 주고 싶은 문구가 있을때
+  const name = "리액트";
+  // 리액트에서 DOM 요소에 스타일을 적용할때는 객체 형태로 넣어야한다
+  const style = {
+    //background-color는 backgroundColor와 같이 -가 사라직ㅎ 카멜 표기법으로 작성됩니다
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: 16, // 단위를 생략하면 px로 지정됩니다.
+  };
+  return <div style={style}>{name}</div>;
 }
 export default App;
