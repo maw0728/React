2f3695084c4bc206295e1dbce71295d77643b751
maw0728 <maw0728@gmail.com>
2020-08-21 21:02:55 +0900

    2.4.3 If 문 대신 조건부 연산자

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -2,10 +2,10 @@ import React /*,{ Fragment } Fragment 사용시*/ from "react";
 
 function App() {
   const name = "리액트";
+  //리액트는 if문 대신 조건부 연사자를 사용해야한다
   return (
     <>
-      <h1>{name} 안녕!</h1>
-      <h2>잘 작동하니?</h2>
+      {name === "리액트" ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다</h2>}
     </>
   );
 }
