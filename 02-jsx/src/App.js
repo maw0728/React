bcf119443878a4a846347659adefe25e5069471a
maw0728 <maw0728@gmail.com>
2020-08-22 04:11:21 +0900

    2.4.8  꼭 닫아야 하는 태그

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 
 function App() {
   const name = "리액트";
-  // 리액트에서 CSS 클래스를 사용할때는 className을 사용
-  return <div className="react">{name}</div>;
+  // JSX는 태그를 닫지 않으면 오류가 발생한다
+  return (
+    <>
+      <div className="react">{name}</div>
+      <input />
+    </>
+  );
 }
 export default App;
