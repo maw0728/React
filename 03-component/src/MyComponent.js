f7326c875ec8f976fe3c2e4838675fb90d430612
maw0728 <maw0728@gmail.com>
2020-08-22 05:20:31 +0900

    3.3.3 props 기본값 설정: defaultProps

--- 03-component/src/MyComponent.js
+++ 03-component/src/MyComponent.js
@@ -4,4 +4,9 @@ const MyComponent = (props) => {
   return <div>안녕하세요, 제 이름은 {props.name}입니다</div>;
 };
 
+// 기본값을 설정하기
+MyComponent.defaultProps = {
+  name: "기본 이름",
+};
+
 export default MyComponent;
