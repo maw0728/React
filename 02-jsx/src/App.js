13f45803cb8af7f3f743f696cc8574748f90cb7c
maw0728 <maw0728@gmail.com>
2020-08-22 04:08:03 +0900

    2.4.7 class 대신 className

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   const name = "리액트";
-  // 리액트에서 DOM 요소에 스타일을 적용할때는 객체 형태로 넣어야한다
-  const style = {
-    //background-color는 backgroundColor와 같이 -가 사라직ㅎ 카멜 표기법으로 작성됩니다
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: "48px",
-    fontWeight: "bold",
-    padding: 16, // 단위를 생략하면 px로 지정됩니다.
-  };
-  return <div style={style}>{name}</div>;
+  // 리액트에서 CSS 클래스를 사용할때는 className을 사용
+  return <div className="react">{name}</div>;
 }
 export default App;
