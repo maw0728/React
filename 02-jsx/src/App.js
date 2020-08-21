e0d1c5daad31fdbd969c3d7364c0df71d0726142
maw0728 <maw0728@gmail.com>
2020-08-21 21:44:06 +0900

    2.4.5 undefined를 렌더링하지 않기

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 
 function App() {
-  const name = "뤼액트";
-  return (
-    /*
-    null을 렌더링하면 아무것도 보여 주지 않습니다
-    <>
-      {name === "리액트" ? <h1>리액트입니다.</h1> : null}
-    </>
-    */
-    // 리액트에서 false를 렌더링할 때는 null과 마찬가지로 아무것도 나타내지 않는다
-    <>{name === "리액트" && <h1>리액트입니다.</h1>}</>
-  );
+  const name = undefined;
+  // 잘못된예시
+  // return name; 함수에서 undefined만 반환하여 렌더링 하면 안 된다
+
+  // 작동가능한 예시
+  // return name || "값이 undefined입니다"; OR 연산자를 이용하여 값이 undfined때 사용할 값을 지정해주면 된다
+  // return <div>{name}</div>; JSX 내부에서 undefined를 렌더링하는 것은 괜찮습니다
+  return <div>{name || "리액트"}</div>; // name 값이 undefined일 때 보여 주고 싶은 문구가 있을때
 }
 export default App;
