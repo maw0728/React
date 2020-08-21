5e0b91cae35be14666f06b33f8a1ecfc550c668f
maw0728 <maw0728@gmail.com>
2020-08-21 21:36:22 +0900

    2.4.4 AND 연산자(&&)를 사용한 조건부 렌더링

--- 02-jsx/src/App.js
+++ 02-jsx/src/App.js
@@ -1,12 +1,16 @@
-import React /*,{ Fragment } Fragment 사용시*/ from "react";
+import React from "react";
 
 function App() {
-  const name = "리액트";
-  //리액트는 if문 대신 조건부 연사자를 사용해야한다
+  const name = "뤼액트";
   return (
+    /*
+    null을 렌더링하면 아무것도 보여 주지 않습니다
     <>
-      {name === "리액트" ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다</h2>}
+      {name === "리액트" ? <h1>리액트입니다.</h1> : null}
     </>
+    */
+    // 리액트에서 false를 렌더링할 때는 null과 마찬가지로 아무것도 나타내지 않는다
+    <>{name === "리액트" && <h1>리액트입니다.</h1>}</>
   );
 }
 export default App;
