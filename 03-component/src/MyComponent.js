52be07eb5a2f5d1e361147b642f1c2be8ee51def
maw0728 <maw0728@gmail.com>
2020-08-22 15:51:37 +0900

    3.3.7 클래스형 컴포넌트에서 props 사용하기

--- 03-component/src/MyComponent.js
+++ 03-component/src/MyComponent.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      안녕하세요, 제 이름은 {name}입니다. <br />
-      children 값은 {children}
-      입니다.
-      <br />
-      제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    </div>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "기본 이름",
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
+  render() {
+    const { name, favoriteNumber, children } = this.props; // 비구조화 할당
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다
+        <br />
+        children 값은 {children}입니다. <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다
+      </div>
+    );
+  }
+}
 
+/*
+기본 방식
 MyComponent.defaultProps = {
   name: "기본 이름",
 };
 
-// 필수 지정을 하게 되면 값이 다르거나 없을 경우 브라우저 콘솔창에 경고 메시지 출력
 MyComponent.propTypes = {
   name: PropTypes.string,
   favoriteNumber: PropTypes.number.isRequired,
 };
-
+*/
 export default MyComponent;
