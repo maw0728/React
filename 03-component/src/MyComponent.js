aa543262830dd85e335ffc9a95f398336eaf9d2f
maw0728 <maw0728@gmail.com>
2020-08-22 15:20:52 +0900

    3.3.6.1 isRequired를 사용하여 필수 propTypes 설정

--- 03-component/src/MyComponent.js
+++ 03-component/src/MyComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다. <br />
       children 값은 {children}
       입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
@@ -14,9 +16,11 @@ const MyComponent = ({ name, children }) => {
 MyComponent.defaultProps = {
   name: "기본 이름",
 };
-// prop 검증 props 타입이 일치하지 않을경우 값은 출력되지만 브라우저 콘솔창에 경고메시지 출력
+
+// 필수 지정을 하게 되면 값이 다르거나 없을 경우 브라우저 콘솔창에 경고 메시지 출력
 MyComponent.propTypes = {
   name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
