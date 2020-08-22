7c6a7a417fb295d927896ed31e23365dcd1c51b8
maw0728 <maw0728@gmail.com>
2020-08-23 02:34:33 +0900

    4.2.1.1 컴포넌트 생성

---
+++ 04-event/src/EventPractice.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
