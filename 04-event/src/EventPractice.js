67e69601c4b120966280bf737ba320576b3ef2c3
maw0728 <maw0728@gmail.com>
2020-08-24 08:33:45 +0900

    4.2.5 onKeyPress 이벤트 핸들링

--- 04-event/src/EventPractice.js
+++ 04-event/src/EventPractice.js
@@ -13,6 +13,11 @@ class EventPractice extends Component {
     alert(this.state.username + ":" + this.state.message);
     this.setState({ username: "", message: "" });
   };
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
 
   render() {
     return (
@@ -30,11 +35,8 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
