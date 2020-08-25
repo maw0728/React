8c9a2e562797a06607fefb22194033fed8043997
maw0728 <maw0728@gmail.com>
2020-08-25 10:55:03 +0900

    5.3.4 컴포넌트에 ref 달고 내부 메서드 사용

--- 05-ref/src/App.js
+++ 05-ref/src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import ScrollBox from "./ScrollBox";
 class App extends Component {
   render() {
-    return <ScrollBox />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
   }
 }
 export default App;
